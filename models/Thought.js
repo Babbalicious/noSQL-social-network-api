ac020ce4dc9bc2afbeeb586de94e681b78cb828b
Chris Babb <Chris Babb>
2024-06-22 16:19:16 -0500

    models finished

---
+++ models/Thought.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const { format } = require("date-fns");
+
+const thoughtSchema = new Schema({
+  thoughtText: { 
+    type: String, 
+    required: true, 
+    minlength: 1, 
+    maxlength: 280
+  },
+  createdAt: { 
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => format(timestamp, 'yyyy-MM-dd HH:mm:ss')
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => format(timestamp, 'yyyy-MM-dd HH:mm:ss')
+  }
+})
+
+
+
+module.exports = Thought;
